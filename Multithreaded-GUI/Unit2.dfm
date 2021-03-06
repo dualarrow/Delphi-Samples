258da117f77e34c3a2756b662b498dd05917651e
Andrew Cumming <andrewc@kestral.com.au>
2021-03-06 12:22:07 +0800

    Added thread pooling to sample

--- Multithreaded-GUI/Unit2.dfm
+++ Multithreaded-GUI/Unit2.dfm
@@ -3,7 +3,7 @@ object Form2: TForm2
   Top = 0
   Caption = 'Form2'
   ClientHeight = 215
-  ClientWidth = 330
+  ClientWidth = 465
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,21 +11,44 @@ object Form2: TForm2
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
   PixelsPerInch = 96
   TextHeight = 13
-  object Label1: TLabel
-    Left = 184
-    Top = 52
-    Width = 31
+  object Counter: TLabel
+    Left = 328
+    Top = 26
+    Width = 39
     Height = 13
-    Caption = 'Label1'
+    Caption = 'Counter'
+  end
+  object ThreadCount: TLabel
+    Left = 328
+    Top = 61
+    Width = 63
+    Height = 13
+    Caption = 'ThreadCount'
+  end
+  object Label1: TLabel
+    Left = 247
+    Top = 26
+    Width = 67
+    Height = 13
+    Caption = 'Work Counter'
+  end
+  object Label2: TLabel
+    Left = 248
+    Top = 61
+    Width = 66
+    Height = 13
+    Caption = 'Thread Count'
   end
   object Button1: TButton
-    Left = 72
-    Top = 40
-    Width = 75
+    Left = 35
+    Top = 21
+    Width = 143
     Height = 25
-    Caption = 'Button1'
+    Caption = 'Run with no thread pool'
     TabOrder = 0
     OnClick = Button1Click
   end
@@ -36,5 +59,14 @@ object Form2: TForm2
     Height = 21
     TabOrder = 1
     Text = 'Edit1'
+  end
+  object Button2: TButton
+    Left = 35
+    Top = 56
+    Width = 143
+    Height = 25
+    Caption = 'Run with thread pool'
+    TabOrder = 2
+    OnClick = Button2Click
   end
 end
--- Multithreaded-GUI/Unit2.pas
+++ Multithreaded-GUI/Unit2.pas
@@ -3,19 +3,30 @@ unit Unit2;
 interface
 
 uses
+  Threading,
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm2 = class(TForm)
     Button1: TButton;
-    Label1: TLabel;
+    Counter: TLabel;
     Edit1: TEdit;
+    Button2: TButton;
+    ThreadCount: TLabel;
+    Label1: TLabel;
+    Label2: TLabel;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
+    FThreadPool: TThreadPool;
+    procedure AddCount;
+    procedure DecCount;
   end;
 
 var
@@ -24,27 +35,114 @@ var
 implementation
 
 uses
-  Threading;
+  SyncObjs;
 
 {$R *.dfm}
 
+var
+  gCritical: TCriticalSection;
+  gThreadCounter: integer = 0;
+  gCount: integer;
+
 procedure TForm2.Button1Click(Sender: TObject);
 begin
-  TTask.Run(
-    procedure
+  TTask.Run(procedure
     begin
-      for var i:=1 to 100 do
+      gCount := 0;
+      for var threads := 1 to 30 do
       begin
-        TThread.Synchronize(nil,
+        TTask.Run(
           procedure
           begin
-            Label1.Caption := i.ToString;
+            AddCount;
+            for var i:=1 to 100 do
+            begin
+              TThread.Synchronize(nil,
+                procedure
+                begin
+                  inc(gCount);
+                  Counter.Caption := gCount.ToString;
+                end
+              );
+              Sleep(200);
+            end;
+            DecCount;
           end
         );
-        Sleep(100);
+        Sleep(10);
       end;
     end
   );
+end;
+
+procedure TForm2.Button2Click(Sender: TObject);
+begin
+  TTask.Run(procedure
+    begin
+      gCount := 0;
+      for var threads := 1 to 30 do
+      begin
+        TTask.Run(
+          procedure
+          begin
+            AddCount;
+            for var i:=1 to 100 do
+            begin
+              TThread.Synchronize(nil,
+                procedure
+                begin
+                  inc(gCount);
+                  Counter.Caption := gCount.ToString;
+                end
+              );
+              Sleep(200);
+            end;
+            DecCount;
+          end,
+          FThreadPool
+        );
+      end;
+    end
+  );
+end;
+
+procedure TForm2.AddCount;
+begin
+  gCritical.Enter;
+  inc(gThreadCounter);
+  TThread.Synchronize(nil,
+    procedure
+    begin
+      ThreadCount.Caption := gThreadCounter.ToString;
+    end
+  );
+  gCritical.Leave;
+end;
+
+procedure TForm2.DecCount;
+begin
+  gCritical.Enter;
+  dec(gThreadCounter);
+  TThread.Synchronize(nil,
+    procedure
+    begin
+      ThreadCount.Caption := gThreadCounter.ToString;
+    end
+  );
+  gCritical.Leave;
+end;
+
+procedure TForm2.FormCreate(Sender: TObject);
+begin
+  gCritical := TCriticalSection.Create;
+  FThreadPool := TThreadPool.Create;
+  FThreadPool.SetMaxWorkerThreads(5);
+end;
+
+procedure TForm2.FormDestroy(Sender: TObject);
+begin
+  FThreadPool.Free;
+  gCritical.Free;
 end;
 
 end.
